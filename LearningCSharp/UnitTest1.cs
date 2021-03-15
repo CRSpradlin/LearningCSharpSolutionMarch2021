d419577bd9c9c46481a87213735dd61866c91d5b
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-15 15:40:32 -0400

    Add project files.

---
+++ LearningCSharp/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace LearningCSharp
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Given (Arrange)clean and pure world.
+            int a = 10, b = 20, answer;
+
+            // When (Act) stimulate the code way
+            answer = a + b;
+            // Then (Assert) observe what happened.
+            Assert.Equal(30, answer);
+        }
+        [Theory]
+        [InlineData(2,2,4)]
+        [InlineData(10,5, 15)]
+        [InlineData(12,2,14)]
+        public void CanAddTwoNumbers(int a, int b, int expected)
+        {
+            int sum = a + b;
+            Assert.Equal(expected, sum);
+        }
+    }
+}
