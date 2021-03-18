8d64b326879120d4b72cdca5e3fec86f045df41b
Christopher Spradlin <github@crspradlin.org>
2021-03-18 12:38:19 -0400

    added mess with its for pass by value/reference

---
+++ LearningCSharp/StuffAboutMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LearningCSharp
+{
+    public class StuffAboutMethods
+    {
+
+        [Fact]
+        public void RefAndVal()
+        {
+            int age = 12;
+            MessWithIt(age);
+            Assert.Equal(12, age);
+        }
+
+        [Fact]
+        public void RefAndVal2()
+        {
+            var dinner = new Dinner { MainCourse = "Palek Paneer" };
+            MessWithIt(dinner);
+            Assert.Equal("McDonalds", dinner.MainCourse);
+        }
+
+        public void MessWithIt(Dinner x)
+        {
+            x.MainCourse = "McDonalds";
+        }
+
+        public void MessWithIt(int x)
+        {
+            x = x * 2;
+        }
+    }
+}
